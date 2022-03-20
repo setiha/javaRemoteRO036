f6d6da02189b788b3fb89d210586e5912abfd2d7
Tihi <tihamer.sebok@gmail.com>
2022-03-20 08:57:02 +0200

    add pocket and person

--- src/ro/sda/curs9/encapsulation/Person.java
+++ src/ro/sda/curs9/encapsulation/Person.java
@@ -1,4 +1,93 @@
 package ro.sda.curs9.encapsulation;
 
 public class Person {
+    // toate proprietatile unei clase sunt variabile GLOBALE
+
+    private String name; // proprietate clasa
+    private String phone;
+    private int age;
+
+    // constructorul are acelasi nume cu clasa
+    // nu are tip returnat
+    // constructorul default este un constructor fara parametrii si blocul de cod este gol
+    public Person() {}
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(String name, String phone, int age) {
+        // parametrii unei metode / constructor sunt variabile locale in metoda / constructorul respectiv.
+
+        // this -> face referire la proprietatile clasei in care ne aflam
+        // this -> se foloseste in metode, contructori
+        // this -> se foloseste pentru a apela proprietati this.name, metode this.printMoney() sau contructori this()
+
+//        this.name = name;
+
+        this.setName(name);
+
+        // apelam metoda prinMoney() din clasa curenta
+//        this.printMoney();
+
+        // apelam un construtor
+//        this("Catalin");
+
+    }
+
+    public void printMoney() {
+        System.out.println("Persoana cu numele: " + this.name + " are datorie 190 lei");
+    }
+
+    // setter si getter - sunt metode care ne ajuta sa controlam valoarea proprietatilor din clasa
+    public void setName(String name) {
+        // .trim() elimina toate spatiile din string
+        if(name.trim().length() == 0) {
+            System.out.println("Nume invalid");
+        } else {
+            this.name = name;
+        }
+
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void addOneYear() {
+        this.age += 1;
+    }
+
+    @Override
+    public String toString() {
+        return "Person: " +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", age=" + this.customAgeText() +
+                '}';
+    }
+
+    /**
+     * Dar scopul metodei toString() este exact ce avem mai jos.
+     * @return proprietatile din clasa Person sub forma de String
+     */
+    public String printPerson() {
+        return "Person: " +
+                "name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", age=" + this.customAgeText() +
+                '}';
+    }
+
+    private String customAgeText() {
+        if(this.age < 18) {
+            return "Minor";
+        } else {
+            return "Adult";
+        }
+    }
 }
---
+++ src/ro/sda/curs9/encapsulation/Pocket.java
@@ -0,0 +1,25 @@
+package ro.sda.curs9.encapsulation;
+
+public class Pocket {
+    private double money;
+
+    public Pocket(double money) {
+        this.money = money;
+    }
+
+    public double getMoney() {
+        if(this.money <= 10) {
+            return 0;
+        }
+
+        return money;
+    }
+
+    public void setMoney(double money) {
+        if(this.money < -10 || this.money > 3000) {
+            System.out.println("I don't have enough space in my pocket for as much money!");
+        } else {
+            this.money = money;
+        }
+    }
+}
